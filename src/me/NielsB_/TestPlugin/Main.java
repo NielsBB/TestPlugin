ca38e4ef596b46d5dcae2f859376357d25fb15d3
NielsB_ <henksmaak18@gmail.com>
2016-05-18 18:34:34 +0200

    Initial commit

---
+++ src/me/NielsB_/TestPlugin/Main.java
@@ -0,0 +1,29 @@
+package me.NielsB_.TestPlugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        //onEnable
+    }
+
+    @Override
+    public void onDisable() {
+        //onDisable
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("allahuakbar")) {
+            sender.sendMessage(ChatColor.RED + "ALLAHU AKBAR");
+            return true;
+        }
+        return true;
+    }
+}
